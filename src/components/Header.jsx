f738c625aefaab9e950ad2e057262e239dc2979c
sarthakkashyap <sarthakkashyap2621@gmail.com>
2025-07-26 16:55:40 +0530

    nav-toggle added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {motion, useScroll} from 'motion/react';
 
 const Header = () => {
-
+  const[menuOpen, setMenuOpen] = useState(false)
   const {scrollYProgress} = useScroll();
+
+  const handleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  useEffect(() => {
+    if(menuOpen) {
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = "auto"
+    }
+  }, [menuOpen])
   
   return (
     <>
@@ -24,8 +36,24 @@ const Header = () => {
                 <a href="#experience" className='hover:underline'>Exp</a>
                 <a href="#contact" className='hover:underline'>contact</a>
             </div>
-            <div className='text-white lg:hidden text-3xl cursor-pointer'>
+            <div className='text-white lg:hidden text-3xl cursor-pointer' onClick={handleMenu}>
                 ☰
+            </div>
+
+            <div className= {`fixed top-0 right-0 h-screen w-50 bg-[#0a0a0a] text-white rounded-sm shadow-lg p-6 z-[99] transform transition-transform duration-300 ease-in-out
+                ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}> 
+
+                <div className='flex justify-end' >
+                  <button className='text-3xl' onClick={handleMenu}>✕</button>
+                </div>
+
+                <ul className="flex flex-col gap-6 text-xl mt-8 font-mono">
+                  <li><a href="#about" onClick={handleMenu}>about</a></li>
+                 <li><a href="#projects" onClick={handleMenu}>projects</a></li>
+                 <li><a href="#skills" onClick={handleMenu}>skills</a></li>
+                <li><a href="#experience" onClick={handleMenu}>exp</a></li>
+                <li><a href="#contact" onClick={handleMenu}>contact</a></li>
+                </ul>
             </div>
         </div>
     <hr className='border-white'/>
---
+++ src/data/experience.js
@@ -0,0 +1,23 @@
+
+export const experienceData = [
+    {
+        id : 1,
+        date : "Sept 2024 - Nov 2024",
+        name : "iDesign.market",
+        role : "Frontend Intern"
+    },
+     {
+        id : 2,
+        date : "Oct 2024 - Nov 2024",
+        name : "Hacktoberfest",
+        role : "Contributor"
+    },
+     {
+        id : 3,
+        date : "Oct 2024 - Nov 2024",
+        name : "GSSOC Extd",
+        role : "Contributor"
+    }
+
+]
+
