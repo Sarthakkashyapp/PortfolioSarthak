11914d8c42e4714b69a7b937db8a0582e16f5e62
sarthakkashyap <sarthakkashyap2621@gmail.com>
2025-07-26 17:23:22 +0530

    fixed experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -21,8 +21,10 @@ function Experience() {
 
       <motion.div className='w-full flex flex-col items-center justify-center mt-16 mx-6 gap-6 md:px-6 px-1.5'>
         {experienceData.map((experience, index) => (
-          <div key={experience.id} className="flex flex-row items-center justify-between border-2 border-white rounded-xl w-full max-w-5xl px-3 lg:px-12 py-6 
-           hover:bg-gradient-to-r from-[rgba(255,255,255,0.1)] via-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0) transition-transform duration-200 shadow-gray-500 hover:shadow-lg" >
+          <div key={experience.id} className={`flex flex-row items-center justify-between border-2 border-white rounded-xl w-full max-w-5xl px-3 lg:px-12 py-6 
+           ${selectedIndex === experience.id ? 'bg-gradient-to-r from-[rgba(255,255,255,0.1)] via-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0) transition-transform duration-200 shadow-gray-500 shadow-lg': 
+           'hover:bg-gradient-to-r from-[rgba(255,255,255,0.1)] via-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0) transition-transform duration-200 shadow-gray-500 hover:shadow-lg'}`} 
+           onClick={() => handleIndex(experience.id)}>
             <p className='text-sm xl:text-xl'>{experience.date}</p>
             <div className='flex flex-col gap-2 items-end'>
               <p className='xl:text-2xl text-xl'>{experience.name}</p>
