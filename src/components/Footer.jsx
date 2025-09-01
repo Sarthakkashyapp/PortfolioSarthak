4fb4cac38f8b2db73a7dd23db7a82f69af11cb90
sarthakkashyap <sarthakkashyap2621@gmail.com>
2025-09-01 15:34:46 +0530

    typo in resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
           <p className='lg:block hidden'>LinkedIn</p>
           <FaLinkedinIn className='text-white lg:hidden text-3xl cursor-pointer'/>
         </a>
-        <a href="https://drive.google.com/file/d/1wh7BR8pNAso7Qrcykgq-ORHSAjeR7vv9/view?usp=drive_link" className='hover:underline'>
+        <a href="https://drive.google.com/file/d/1uB9kW8kw0d98aiOIDAhpiO1hQmXI97DJ/view?usp=drive_link" className='hover:underline'>
           <p className='lg:block hidden'>Resume</p>
           <img src={resumeicon} alt="icon" className='invert brightness-0 lg:hidden text-3xl cursor-pointer h-7.5 w-8 hover:scale-105'/>
         </a>
